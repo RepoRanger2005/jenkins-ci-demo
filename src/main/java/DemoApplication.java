8297cedad833e00d92c3cfd0fff26b9f42a2b89d
Preetam Reddy <preetamreddy85@gmail.com>
2026-03-16 14:45:37 +0530

    Fixing database auto-config error

--- src/main/java/DemoApplication.java
+++ src/main/java/DemoApplication.java
@@ -3,7 +3,9 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RestController;
 
-@SpringBootApplication
+@SpringBootApplication(exclude = { 
+    org.springframework.boot.autoconfigure.r2dbc.R2dbcAutoConfiguration.class 
+})
 @RestController
 public class DemoApplication {
 
